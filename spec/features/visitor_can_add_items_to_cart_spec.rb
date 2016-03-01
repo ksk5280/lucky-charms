ea29f1f72fbe93facfafd4ed724331331b9e8869
Brennan Holtzclaw <brennanholtzclaw@gmail.com>
2016-03-01 16:25:49 -0700

    Add factory girl to tests
    RSpec tests use Factory Girl to generate test categories and items

--- spec/features/visitor_can_add_items_to_cart_spec.rb
+++ spec/features/visitor_can_add_items_to_cart_spec.rb
@@ -2,17 +2,14 @@ require "rails_helper"
 
 RSpec.feature "visitor can add items to cart" do
   scenario "visitor sees items and total price in cart" do
-    cat1 = Category.create(title: "charms")
-    cat1.items << Item.create(title: "Lucky Penny",
-                              description: "Shiny",
-                              price: 1000,
-                              image: "/images/Penny.png",)
+    category_one = create(:category)
+    category_one.items << create(:item)
 
-    visit "/items"
+    visit "/#{category_one.title}"
     expect(page).to have_content("Cart (0)")
 
     click_on "Add to Cart"
-    expect(page).to have_content("Lucky Penny added to cart!")
+    expect(page).to have_content("Lucky Item 1 added to cart!")
     expect(page).to have_content("Cart (1)")
 
     click_on "Cart"
@@ -20,14 +17,14 @@ RSpec.feature "visitor can add items to cart" do
     expect(current_path).to eq "/cart_items"
 
     within(".cart-items") do
-      expect(page).to have_content("Lucky Penny")
-      expect(page).to have_content("Shiny")
-      expect(page).to have_content("$10.00")
-      expect(page).to have_css("img[src=\"/images/Penny.png\"]")
+      expect(page).to have_content("Lucky Item 1")
+      expect(page).to have_content("1 description")
+      expect(page).to have_content("$0.01")
+      expect(page).to have_css("img[src=\"http://bit.ly/1RlgZV5\"]")
     end
 
     within(".cart-container") do
-      expect(page).to have_content("Total: $10.00")
+      expect(page).to have_content("Total: $0.01")
     end
   end
 end
--- spec/features/visitor_can_browse_items_by_category_spec.rb
+++ spec/features/visitor_can_browse_items_by_category_spec.rb
@@ -2,41 +2,25 @@ require "rails_helper"
 
 RSpec.feature "Visitor can browse items by category" do
   scenario "they see all items in a category" do
-    cat1 = Category.create(title: "charms")
-    cat2 = Category.create(title: "potions")
-    cat1.items.create(title: "Lucky Penny",
-                      description: "Shiny",
-                      price: 1000,
-                      image: "/images/Penny.png",)
-    cat1.items.create(title: "Horse Shoe",
-                      description: "Heavy",
-                      price: 2000,
-                      image: "/images/horseshoe.png",)
-    cat2.items.create(title: "Love Potion #9",
-                      description: "Potent",
-                      price: 5000,
-                      image: "/images/horseshoe.png",)
-    cat2.items.create(title: "Luck Potion",
-                      description: "Lucky",
-                      price: 5000,
-                      image: "/images/horseshoe.png",)
+    category_one = create(:category_with_items)
+    category_two = create(:category_with_items)
 
-    visit "/charms"
+    visit "/#{category_one.title}"
 
     within ".items" do
-      expect(page).to have_content "Lucky Penny"
-      expect(page).to have_content "Horse Shoe"
-      expect(page).to_not have_content "Love Potion #9"
-      expect(page).to_not have_content "Luck Potion"
+      expect(page).to have_content "Lucky Item 2"
+      expect(page).to have_content "Lucky Item 3"
+      expect(page).to_not have_content "Lucky Item 5"
+      expect(page).to_not have_content "Lucky Item 6"
     end
 
-    visit "/potions"
+    visit "/#{category_two.title}"
 
     within ".items" do
-      expect(page).to_not have_content "Lucky Penny"
-      expect(page).to_not have_content "Horse Shoe"
-      expect(page).to have_content "Love Potion #9"
-      expect(page).to have_content "Luck Potion"
+      expect(page).to_not have_content "Lucky Item 2"
+      expect(page).to_not have_content "Lucky Item 3"
+      expect(page).to have_content "Lucky Item 5"
+      expect(page).to have_content "Lucky Item 6"
     end
   end
 end
