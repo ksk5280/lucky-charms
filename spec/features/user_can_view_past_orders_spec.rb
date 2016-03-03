8e9396391d253c7ef27d745e404502c9af7822fa
Kimiko Kano <kskano@gmail.com>
2016-03-03 15:44:11 -0700

    Adds feature test for user orders view
    * Update database schema file for orders

---
+++ spec/features/user_can_view_past_orders_spec.rb
@@ -0,0 +1,32 @@
+require "rails_helper"
+
+RSpec.feature "User can view past orders" do
+  scenario "they see all orders belonging to them" do
+    user = create(:user)
+    order1 = user.orders.create(status: "ordered", created_at: Time.new(2016, 3, 1))
+    order2 = user.orders.create(status: "shipped", created_at: Time.new(2016, 3, 2))
+    order3 = user.orders.create(status: "pending", created_at: Time.new(2016, 3, 3))
+
+    visit root_path
+    click_on "Login"
+    fill_in "Username", with: user.username
+    fill_in "Password", with: "password"
+    click_on "Login"
+
+    visit "/orders"
+
+    within ".orders" do
+      expect(page).to have_content "Order ##{order1.id}"
+      expect(page).to have_content "Status: ordered"
+      expect(page).to have_content "Placed on: March 1, 2016"
+
+      expect(page).to have_content "Order ##{order2.id}"
+      expect(page).to have_content "Status: shipped"
+      expect(page).to have_content "Placed on: March 2, 2016"
+
+      expect(page).to have_content "Order ##{order3.id}"
+      expect(page).to have_content "Status: pending"
+      expect(page).to have_content "Placed on: March 3, 2016"
+    end
+  end
+end
