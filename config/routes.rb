9c04658d98407f16d0bdfba1d8a90adaad479f2b
Kimiko Kano <kskano@gmail.com>
2016-03-02 21:50:01 -0700

    Creates landing page
    * Creates Homes Controller for root path
    * Creates homes#show route and view for landing page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :items, only: [:index, :show]
   resources :cart_items, only: [:create, :destroy]
+  root to: "homes#show"
   get "/cart", to: "cart_items#index"
   get "/:category", to: "categories#show", as: "category"
 end
