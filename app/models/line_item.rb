10d1744bf7b6bea4d6aeb50f8647114c0aaf7a93
Kimiko Kano <kskano@gmail.com>
2016-03-04 17:43:24 -0700

    User can view a single order
    * Create order show view

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,4 +1,14 @@
 class LineItem < ActiveRecord::Base
+  include ActionView::Helpers::NumberHelper
+
   belongs_to :order
   belongs_to :item
+
+  def subtotal
+    (quantity * item.price).to_f / 100
+  end
+
+  def formatted_subtotal
+    number_to_currency(subtotal)
+  end
 end
---
+++ spec/features/user_can_view_single_order_spec.rb
@@ -0,0 +1,52 @@
+require "rails_helper"
+
+RSpec.feature "User can view a single order" do
+  scenario "they see order details" do
+    user = create(:user)
+    order = user.orders.create(status: "ordered",
+                                created_at: Time.new(2016, 3, 1))
+    item1 = create(:item)
+    item2 = create(:item)
+    line_item1 = order.line_items.create(item: item1, quantity: 2)
+    line_item2 = order.line_items.create(item: item2, quantity: 3)
+
+    visit root_path
+    first(:link, "Login").click
+    fill_in "Username", with: user.username
+    fill_in "Password", with: "password"
+    click_on "Login to your account"
+
+    visit "/orders"
+    within ".orders" do
+      expect(page).to have_content "##{order.id}"
+      expect(page).to have_content "ordered"
+      expect(page).to have_content "March 1, 2016"
+    end
+
+    click_link "##{order.id}"
+    within ".items" do
+      expect(page).to have_content item1.title
+      expect(page).to have_content "Qty. #{line_item1.quantity}"
+      expect(page).to have_content "Subtotal: #{line_item1.formatted_subtotal}"
+      expect(page).to have_content item2.title
+      expect(page).to have_content "Qty. #{line_item2.quantity}"
+      expect(page).to have_content "Subtotal: #{line_item2.formatted_subtotal}"
+      expect(page).to have_link item1.title
+      expect(page).to have_link item2.title
+    end
+    expect(page).to have_content "ordered"
+    expect(page).to have_content "Total: $0.05"
+    expect(page).to have_content "Date Submitted: March 1, 2016"
+  end
+
+
+
+    #sad path
+#       If the order was completed or cancelled,
+#       Then I should see a timestamp when the action took place
+    # expect(page).to have_content
+#       And if any of the items in the order were retired from the menu
+
+#       Then they should still be able to access the item page
+#       But they should not be able to add the item to their cart
+end
