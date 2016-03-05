bb34ee9f1a7e53bbe438bea75ea7433f5dbbe504
Kimiko Kano <kskano@gmail.com>
2016-03-04 18:48:28 -0700

    User can view a single order
    Create order show view

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,24 @@
 class Order < ActiveRecord::Base
+  include ActionView::Helpers::NumberHelper
+
   belongs_to :user
   has_many :line_items
   has_many :items, through: :line_items
 
   validates :status, presence: true
+  #status of the order (ordered, paid, cancelled, completed)
+
+  def formatted_date
+    created_at.strftime("%B %-d, %Y")
+  end
+
+  def total
+    line_items.inject(0) do |total, line_item|
+      total + line_item.subtotal
+    end
+  end
+
+  def formatted_total
+    number_to_currency(total)
+  end
 end
--- spec/features/user_can_view_single_order_spec.rb
+++ spec/features/user_can_view_single_order_spec.rb
@@ -2,49 +2,41 @@ require "rails_helper"
 
 RSpec.feature "User can view a single order" do
   scenario "they see order details" do
-    # Background: An existing user that has one previous order
     user = create(:user)
-    order1 = user.orders.create(status: "ordered",
+    order = user.orders.create(status: "ordered",
                                 created_at: Time.new(2016, 3, 1))
     item1 = create(:item)
     item2 = create(:item)
-    line_item1 = order1.line_items.create(item: item1, quantity: 2)
-    line_item2 = order1.line_items.create(item: item2, quantity: 3)
-    #       As an authenticated user
+    line_item1 = order.line_items.create(item: item1, quantity: 2)
+    line_item2 = order.line_items.create(item: item2, quantity: 3)
+
     visit root_path
     first(:link, "Login").click
     fill_in "Username", with: user.username
     fill_in "Password", with: "password"
     click_on "Login to your account"
-    #       When I visit "/orders"
+
     visit "/orders"
-#       Then I should see my past order
     within ".orders" do
-      expect(page).to have_content "##{order1.id}"
+      expect(page).to have_content "##{order.id}"
       expect(page).to have_content "ordered"
       expect(page).to have_content "March 1, 2016"
     end
-#       And I should see a link to view that order
-#       And when I click that link
-    click_link "##{order1.id}"
-#       Then I should see each item that was order with the quantity and line-item subtotals
+
+    click_link "##{order.id}"
     within ".items" do
       expect(page).to have_content item1.title
-      expect(page).to have_content "Quantity: #{line_item1.quantity}"
+      expect(page).to have_content "Qty. #{line_item1.quantity}"
       expect(page).to have_content "Subtotal: #{line_item1.subtotal}"
       expect(page).to have_content item2.title
-      expect(page).to have_content "Quantity: #{line_item2.quantity}"
+      expect(page).to have_content "Qty. #{line_item2.quantity}"
       expect(page).to have_content "Subtotal: #{line_item2.subtotal}"
-      #       And I should see links to each item's show page
-      expect(page).to have_link "#{item1.title}"
-      expect(page).to have_link "#{item2.title}"
+      expect(page).to have_link item1.title
+      expect(page).to have_link item2.title
     end
-    # And I should see the current status of the order (ordered, paid, cancelled, completed)
-    expect(page).to have_content order.status
-    #       And I should see the total price for the order
-    expect(page).to have_content order.total
-#       And I should see the date/time that the order was submitted
-    expect(page).to have_content order.formatted_date
+    expect(page).to have_content "ordered"
+    expect(page).to have_content "Total: $0.05"
+    expect(page).to have_content "Date Submitted: March 1, 2016"
   end
 
 
