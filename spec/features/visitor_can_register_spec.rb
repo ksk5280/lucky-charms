5b6bfb01cfa17eda42d555ed8ea5848b9d9c095c
Erinna Chen <erinna.chen@gmail.com>
2016-03-06 11:40:31 -0700

    Clean up test organization

--- spec/features/visitor_can_register_spec.rb
+++ spec/features/visitor_can_register_spec.rb
@@ -1,71 +1,35 @@
 require "rails_helper"
 
-RSpec.feature "visitor can log in" do
-  scenario "visitor logs in and sees dashboard" do
-    visit "/"
+RSpec.feature "visitor can create account" do
+  context "enters valid username and password" do
+    scenario "visitor logs in and sees dashboard" do
+      visit "/"
+      first(:link, "Create Account").click
 
-    expect(page).to have_content("Login")
+      fill_in "Username", with: "brennan"
+      fill_in "Password", with: "password"
 
-    first(:link, "Create Account").click
-
-    fill_in "Username", with: "brennan"
-    fill_in "Password", with: "password"
-
-    click_button "Create Account"
-    expect(current_path).to eq "/dashboard"
-    expect(page).to have_content("Logged in as brennan")
-    expect(page).to have_content("Your Orders")
-    expect(page).to have_content("Logout")
-    expect(page).to_not have_content("Login")
-  end
-end
-
-RSpec.feature "logged in user sees items in cart put there before logging in" do
-  scenario "visitor turned user sees items in cart" do
-    category = create(:category_with_items)
-    User.create(username: "brennan", password: "password")
-
-    visit "/items"
-    category.items.each { |item| expect(page).to have_content(item.title) }
-
-    within ".items" do
-      first(:button, "Add to Cart").click
+      click_button "Create Account"
+      expect(current_path).to eq "/dashboard"
+      expect(page).to have_content("Logged in as brennan")
+      expect(page).to have_content("Your Orders")
+      expect(page).to have_content("Logout")
+      expect(page).to_not have_content("Login")
     end
-
-    visit "/cart"
-    expect(page).to have_content(category.items.first.title)
-
-    click_on "Login"
-
-    fill_in "Username", with: "brennan"
-    fill_in "Password", with: "password"
-    click_on "Login to your account"
-
-    visit "/cart"
-
-    expect(page).to have_content(category.items.first.title)
-    expect(page).to_not have_content(category.items[1].title)
-    expect(page).to_not have_content(category.items.last.title)
   end
-end
 
-RSpec.feature "clicking logout ends session and changes login/out link" do
-  scenario "user turns visitor and sees logout link change" do
-    User.create(username: "brennan", password: "password")
+  context "enters an existing username" do
+    scenario "sees error message and create account page" do
+      user = create(:user)
 
-    visit "/"
-    first(:link, "Login").click
+      visit "/"
+      first(:link, "Create Account").click
+      fill_in "Username", with: user.username
+      fill_in "Password", with: "password"
+      click_button "Create Account"
 
-    fill_in "Username", with: "brennan"
-    fill_in "Password", with: "password"
-    click_on "Login to your account"
-
-    expect(page).to have_content("Logout")
-    expect(page).to_not have_content("Login")
-
-    click_on "Logout"
-
-    expect(page).to have_content("Login")
-    expect(page).to_not have_content("Logout")
+      expect(page).to have_content "Invalid account details. Please try again."
+      expect(page).not_to have_content user.username
+    end
   end
 end
