678f0870a6feb90f856caae86a4c03387060e2b9
Brennan Holtzclaw <brennanholtzclaw@gmail.com>
2016-03-07 09:21:09 -0700

    Building seed file
    * Add categories with items
    * Add Charms category
    * Add Potions category
    * Add Clothes category
    * Add three items to each category
    * Add image paths for all items and categories
    * Move images and add to seed file
    * Remove '/images/' part of image paths
    * Move seeds from seedfile to seeds.rb

--- seedfile.rb
+++ seedfile.rb
@@ -1,11 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-
 charms = Category.create(title: "Charms", created_at: Time.now, image: "charms.jpg")
 potions = Category.create(title: "Potions", created_at: Time.now, image: "felix_felicis.jpg")
 clothes = Category.create(title: "Clothes", created_at: Time.now, image: "lucky_socks.jpg")
