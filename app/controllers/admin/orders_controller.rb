7563f1777da5e6787675d5b36e1353ba170b02a8
Erinna Chen <erinna.chen@gmail.com>
2016-03-07 10:34:16 -0700

    Fix styling on multiple pages
    * users#show
    * home#show
    * orders#show
    * orders#index
    * admin/orders#index
    * admin/orders#show
    * admin/dashboard#index
    * navbar
    * 404 page

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -6,8 +6,10 @@ class Admin::OrdersController < Admin::BaseController
   def index
     if Order.statuses.include?(params[:status])
       @orders = Order.where(status: params[:status]).order(id: :asc)
+      @status = params[:status].capitalize
     else
       @orders = Order.all.order(id: :asc)
+      @status = "All"
       flash.now[:error] = "Status is invalid"
       render "admin/dashboard/index"
     end
