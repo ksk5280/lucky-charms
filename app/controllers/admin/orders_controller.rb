4298f5f17a144957052695a1c0da2a72cd6bd18a
Erinna Chen <erinna.chen@gmail.com>
2016-03-07 18:54:13 -0700

    Update order paths for admin

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -7,10 +7,12 @@ class Admin::OrdersController < Admin::BaseController
     if Order.statuses.include?(params[:status])
       @orders = Order.where(status: params[:status]).order(id: :asc)
       @status = params[:status].capitalize
-    else
+    elsif !params[:status]
       @orders = Order.all.order(id: :asc)
       @status = "All"
+    else
       flash.now[:error] = "Status is invalid"
+      @orders = Order.all.order(id: :asc)
       render "admin/dashboard/index"
     end
   end
