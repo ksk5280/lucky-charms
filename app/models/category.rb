76150ebbc7f67ed5b3ed02baf69316765baea16e
Kimiko Kano <kskano@gmail.com>
2016-03-09 10:11:52 -0700

    Categories have a default image

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,5 @@ class Category < ActiveRecord::Base
   has_many :category_items
   has_many :items, through: :category_items
   validates :title, presence: true, uniqueness: true
+  validates :image, presence: true
 end
