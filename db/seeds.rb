3241fc52bde153d0fd87feaddf520ddcf40cb4e6
Erinna Chen <erinna.chen@gmail.com>
2016-03-09 23:45:14 -0700

    Add image to seedfile

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,7 +19,7 @@ Item.create(title: "Peyton Manning Tighty Whities", description: "We're not real
 
 Item.create(title: "Uncatchable Leprechaun", description: "You're not having luck with the weather, but if you're inside, try to nab this little guy and get his gold!", price: 9999, image: File.new("#{Rails.root}/app/assets/images/leprechaun_face.png"), categories: [Category.find_by(title: "Weather")])
 Item.create(title: "Pot O' Gold", description: "After a good rain, you were lucky enough to find the pot of gold at the end of the rainbow!", price: 99999, image: File.new("#{Rails.root}/app/assets/images/pot_o_gold.png"), categories: [Category.find_by(title: "Weather")])
-Item.create(title: "Four-Leaf Clover", description: "The sun is shining so brightly that spotting this little guy wasn't a matter of luck!", price: 999, image: File.new("#{Rails.root}/app/assets/images/sunshine.png"), categories: [Category.find_by(title: "Weather")])
+Item.create(title: "Four-Leaf Clover", description: "The sun is shining so brightly that spotting this little guy wasn't a matter of luck!", price: 999, image: File.new("#{Rails.root}/app/assets/images/four-leaf-clover.png"), categories: [Category.find_by(title: "Weather")])
 
 User.create(username: "johndoe", password: "password", role: 0, first_name: "John", last_name: "Doe", address: "1510 Blake Street")
 User.create(username: "janedoe", password: "password", role: 0, first_name: "Jane", last_name: "Doe", address: "1511 Blake Street")
