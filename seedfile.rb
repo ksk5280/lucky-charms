36e88fdb958a3727064fa9558f839bc98e105fcd
Brennan Holtzclaw <brennanholtzclaw@gmail.com>
2016-03-09 17:08:56 -0700

    Reconfigure seed syntax and add weather category and items

--- seedfile.rb
+++ seedfile.rb
@@ -1,6 +1,7 @@
 charms = Category.create(title: "Charms", created_at: Time.now, image: "charms.jpg")
 potions = Category.create(title: "Potions", created_at: Time.now, image: "felix_felicis.jpg")
 clothes = Category.create(title: "Clothes", created_at: Time.now, image: "lucky_socks.jpg")
+weather = Category.create(title: "Weather", created_at: Time.now, image: "sunshine.png")
 
 charms.items << Item.create(title: "Lucky Penny", description: "A shiny heads-up penny you found in the road!", price: 999 , image: "Penny.png")
 charms.items << Item.create(title: "Horseshoe", description: "Keep the open side pointed up so the luck doesn't drain out!", price: 1499, image: "horseshoe.png")
@@ -14,7 +15,11 @@ clothes.items << Item.create(title: "Lucky Pat Wey Jersey", description: "A game
 clothes.items << Item.create(title: "Game Day Sock", description: "Worn by one fan during every home game played on a Thursday in the month of September, but only for night games - and his team won EVERY(both) TIME!", price: 1099, image: "lucky_game_socks.jpg")
 clothes.items << Item.create(title: "Peyton Manning Tighty Whities", description: "We're not really sure how these were obtained. They sure smell like they were game-worn. Gotta be lucky, right?", price: 99999, image: "tighty_whities.jpg")
 
+weather.items << Item.create(title: "Uncatchable Leprechaun", description: "You're not having luck with the weather, but if you're inside, try to nab this little guy and get his gold!", price: 9999, image: "pot_o_gold.png")
+weather.items << Item.create(title: "Pot O' Gold", description: "After a good rain, you were lucky enough to find the pot of gold at the end of the rainbow!", price: 99999, image: "pot_o_gold.png")
+weather.items << Item.create(title: "Four-Leaf Clover", description: "The sun is shining so brightly that spotting this little guy wasn't a matter of luck!", price: 999, image: "sunshine.png")
+
 User.create(username: "johndoe", password: "password", role: 0, first_name: "John", last_name: "Doe", address: "1510 Blake Street")
 User.create(username: "janedoe", password: "password", role: 0, first_name: "Jane", last_name: "Doe", address: "1511 Blake Street")
-User.create(username: "user", password: "password", role: 0, first_name: "Tim", last_name: "Finnigan", address: "1234 Walker Street")
+User.create(username: "user", password: "password", role: 0, first_name: "Tim", last_name: "Finnigan", address: "1234 Walken Street")
 User.create(username: "admin", password: "password", role: 1, first_name: "Anon", last_name: "Ymous", address: "The cloud")
